67719b238e9d48edc80147cdb5e5509135dcdf6c
Roberto Gabriel <rg.martinsdasilva@gmail.com>
2023-08-21 14:46:33 +0000

    Estrutura IF

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//condicional
+var flag = 50;
+
+if(flag>50){
+    Console.WriteLine("Maior");    
+}
+else{
+    Console.WriteLine("não é maior");
+}
+switch (flag) {
+    case 50:
+    Console.WriteLine("Valor igual 50");
+    break;
+}
